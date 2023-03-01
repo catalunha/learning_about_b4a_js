735ef9e7f39496755811e9646abffe9abd81d7a8
catalunha <catalunha.mj@gmail.com>
2023-03-01 14:31:31 -0300

    rev

---
+++ README.md
@@ -0,0 +1,56 @@
+# Como executar
+```
+catalunha@pop-os:~/myapp/learning_about_b4a_js$ node index.js
+open...
+printContacts+++
+{
+  typeString: 'Genre01',
+  typeBoolean: true,
+  typeNumber: 1,
+  typeDateTime: { __type: 'Date', iso: '2022-12-14T19:53:00.000Z' },
+  typeArray: [ 'a', '1' ],
+  createdAt: '2022-12-14T19:53:49.114Z',
+  updatedAt: '2022-12-14T19:53:49.114Z',
+  objectId: 'KinHJTTxv5'
+}
+2022-12-14T19:53:00.000Z
+object
+{
+  typeString: 'Genre02',
+  typeBoolean: false,
+  typeNumber: 2,
+  typeDateTime: { __type: 'Date', iso: '2022-12-14T20:53:00.000Z' },
+  typeArray: [ 'b', '2' ],
+  createdAt: '2022-12-14T19:53:49.403Z',
+  updatedAt: '2022-12-14T19:53:49.403Z',
+  objectId: 'Twv7hnpF9E'
+}
+2022-12-14T20:53:00.000Z
+object
+{
+  typeString: 'Genre03',
+  typeBoolean: true,
+  typeNumber: 3,
+  typeDateTime: { __type: 'Date', iso: '2022-12-14T21:53:00.000Z' },
+  typeArray: [ 'c', '3' ],
+  createdAt: '2022-12-14T19:53:49.691Z',
+  updatedAt: '2022-12-14T19:53:49.691Z',
+  objectId: 'IKGxFAScET'
+}
+2022-12-14T21:53:00.000Z
+object
+{
+  typeDateTime: { __type: 'Date', iso: '2023-03-01T15:48:00.000Z' },
+  typeArray: [ '1', 'um' ],
+  typeNumber: 4,
+  typeString: 'um',
+  typeBoolean: true,
+  createdAt: '2023-03-01T14:48:41.507Z',
+  updatedAt: '2023-03-01T15:04:19.239Z',
+  objectId: 'TIUzNT3L8d'
+}
+2023-03-01T15:48:00.000Z
+object
+printContacts---
+```
+
--- index.js
+++ index.js
@@ -1,71 +1,60 @@
-console.log('Start');
-// In a node.js environment
 const Parse = require('parse/node');
-Parse.initialize("x4uHx8hJECtAPiKo4Z2f8IK9OUKRtcuUBBaUBHM9", "BzNK4dwZ1UvFcgsyQum6jktmOFUrxr4EcgaS4Mp7");
-//javascriptKey is required only if you have it on server.
+Parse.serverURL = 'https://learningaboutb4a.b4a.io';
+Parse.initialize('x4uHx8hJECtAPiKo4Z2f8IK9OUKRtcuUBBaUBHM9','BzNK4dwZ1UvFcgsyQum6jktmOFUrxr4EcgaS4Mp7');
 
-Parse.serverURL = 'https://parseapi.back4app.com'
+const main = async ()=>{
+  const query = new Parse.Query('Genre');
+  // query.notEqualTo('isMan',true);
+  // query.equalTo('birthDate',null);
+  query.exists('typeDateTime');
+  const subscribe = await query.subscribe();
 
-function getOne() {
-  console.log('getOne');
-  const Genre = Parse.Object.extend("Genre");
-  const query = new Parse.Query(Genre);
-  // query.equalTo("typeString", 'Genre01');
-  // query.first().then((genre) => {
-  query.get('Twv7hnpF9E').then((genre) => {
-    // The object was retrieved successfully.
-    const typeString = genre.get("typeString");
-    console.log('getOne.typeString');
-    console.log(typeString);
+  let contacts = {};
+  const printContacts = ()=>{
+    console.log('printContacts+++');
+    Object.keys(contacts).forEach(id=>{
+      console.log(contacts[id].toJSON());
+      console.log(contacts[id].get('typeDateTime'))
+      console.log(typeof contacts[id].get('typeDateTime'))
+    });
+    console.log('printContacts---');
+  };
 
-  }, (error) => {
-    console.log('teste3');
-    console.log(error);
-    // The object was not retrieved successfully.
-    // error is a Parse.Error with an error code and message.
+  subscribe.on('open', async ()=>{
+    console.log('open...');
+    contacts = (await query.find()).reduce((contacts,contact)=>({
+      ...contacts,
+      [contact.id]: contact
+    }),contacts);
+    printContacts();
   });
-}
-async function getOne2() {
-  console.log('getOne2');
-  const Genre = Parse.Object.extend("Genre");
-  const query = new Parse.Query(Genre);
-    try {
-      // query.equalTo("typeString", 'Genre02');
-    const result = await query.first();
-    // const result = await query.get('Twv7hnpF9E');
-    const typeString = result.get("typeString");
-    console.log('getOne2.typeString');
-    console.log(typeString);
-  } catch (error) {
-    console.error('Error while fetching Genre', error);
-  }
-}
-getOne();
-getOne2();
-console.log('End');
-
-
-// (async () => {
-//   const Genre = Parse.Object.extend('Genre');
-//   const query = new Parse.Query(Genre);
-//   // You can also query by using a parameter of an object
-//   // query.equalTo('objectId', 'xKue915KBG');
-//   try {
-//     const results = await query.find();
-//     for (const object of results) {
-//       // Access the Parse Object attributes using the .GET method
-//       const typeString = object.get('typeString')
-//       const typeBoolean = object.get('typeBoolean')
-//       const typeNumber = object.get('typeNumber')
-//       const typeArray = object.get('typeArray')
-//       const typeDateTime = object.get('typeDateTime')
-//       console.log(typeString);
-//       console.log(typeBoolean);
-//       console.log(typeNumber);
-//       console.log(typeArray);
-//       console.log(typeDateTime);
-//     }
-//   } catch (error) {
-//     console.error('Error while fetching Genre', error);
-//   }
-// })();
+  subscribe.on('create',(data)=>{
+    console.log(`Data: ${data.id} | create...`);
+    contacts[data.id]=data;
+    printContacts();
+  });
+  subscribe.on('update',(data)=>{
+    console.log(`Data: ${data.id} | update...`);
+    contacts[data.id]=data;
+    printContacts();
+  });
+  subscribe.on('enter',(data)=>{
+    console.log(`Data: ${data.id} | enter...`);
+    contacts[data.id]=data;
+    printContacts();
+  });
+  subscribe.on('leave',(data)=>{
+    console.log(`Data: ${data.id} | leave...`);
+    delete contacts[data.id];
+    printContacts();
+  });
+  subscribe.on('delete',(data)=>{
+    console.log(`Data: ${data.id} | delete...`);
+    delete contacts[data.id];
+    printContacts();
+  });
+  subscribe.on('closed',()=>{
+    console.log('closed...');
+  });  
+};
+main();
